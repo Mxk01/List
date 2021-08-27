ce5fb19dd51180ea28a308afe17aeaa38cabdcd6
Mxk01 <secretcoderz@yahoo.com>
2021-08-27 11:08:17 +0300

    =

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
  import './App.css';
-
+ import {useState,useEffect} from 'react';
+ // not default export
+ import {items} from './items';
 function App() {
+
+  // myItems is different from items to avoid naming conflicts
+  let [myItems,setMyItems] = useState(items); 
+  let [cleared,setCleared] = useState(false);
+  // useEffect(()=>{
+    // if(cleared)
+    // {
+    //  setMyItems(localStorage.getItem('empty'))
+    // }
+    // },[])
+  let clear=  ()=> {
+    // clear the list of items   E.G change from [{key:1,name:'pizza'}] to []
+     setMyItems([]) 
+    //  setCleared(true)
+    //  localStorage.getItem('empty',[])
+  }
+
+  let deleteItem = (key) => (
+     // fuck didn't pass item to fitler
+     // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+     setMyItems(myItems.filter((item)=>item.key!==key))
+    
+  )
   return (
     <div className="App">
-    
+     {
+       myItems.map((item)=> {
+         return(
+           <div key={item.key}> 
+       <li className="item"    >
+         {item.key} . {item.name}
+         </li>
+        <button onClick={()=>deleteItem(item.key)}>Delete item</button>
+        </div>
+       )
+     
+    })}
+     <button onClick={clear}>Clear List</button>
     </div>
   );
 }
---
+++ src/items.js
@@ -0,0 +1,22 @@
+export let items = [
+    {
+       key:1,
+       name:'pizza'
+    },
+    {
+        key:2,
+        name:'hamburger'
+    },
+    {
+        key:3,
+        name:'chocolate'
+    },
+    {
+        key:4,
+        name:'parfait'
+    },
+    {
+        key:5,
+        name:'vodka'
+    }
+]
